cf21bfb3c2c48d8f503314a832c77dc0edb2662f
Pranjal <pranjal.point@gmail.com>
2025-08-20 02:17:59 +0530

    s1aapi revived

--- Empire-S1API/GeneralSetup/GeneralSetup.cs
+++ Empire-S1API/GeneralSetup/GeneralSetup.cs
@@ -24,7 +24,7 @@ namespace Empire
                 //Send the mod intro and debt message to player
                 NPC1.Get<UncleNelson>().SendTextMessage("Listen up, kid. I'm out of the business now, but I still got some connections. I'll put in a good word for you. How far you go is up to you. Make good relations and do good business with the dealers and you'll climb up the ladders. That'll get you access to some top tier deals.");
                 NPC1.Get<UncleNelson>().SendTextMessage("But I owed some cash to the wrong people. Cartels and the wrong sort. They'll expect you to pay it off since you're the man of the family now. They not the negotiating type, if you know what I mean. That means you need to step up son. Don't let me down. I don't want to see you in a body bag. You got that?");
-                NPC1.Get<UncleNelson>().SendTextMessage("I'll help you out however I can. Don't come looking for me though. Heh. Take care son.");
+                NPC1.Get<UncleNelson>().SendTextMessage("I'll help you out however I can. Don't come looking for me though. Heh. I'll call from time to time. Take care son.");
             }
         }
     }
--- Empire-S1API/NPC/Data/DealerSaveData.cs
+++ Empire-S1API/NPC/Data/DealerSaveData.cs
@@ -3,24 +3,27 @@ using Empire;
 using System;
 using System.Collections.Generic;
 
-namespace Empire;
-
-public class DealerSaveData
+namespace Empire
 {
-    public int Reputation; // The dealer's reputation
-    public int ShippingTier; // The dealer's shipping tier
-    public int DealsCompleted; // Number of deals completed with this dealer
-    public List<Drug>? UnlockedDrugs; // List of unlocked drug types with unlocked qualities and effects only
-    public float DebtRemaining; // Remaining debt to the dealer
-    public float DebtPaidThisWeek; // Amount paid to the dealer this week
 
-    public DealerSaveData()
+    public class DealerSaveData
     {
-        Reputation = 0;
-        ShippingTier = 0;
-        DealsCompleted = 0;
-        UnlockedDrugs = new List<Drug>();
-        DebtRemaining = 0f;
-        DebtPaidThisWeek = 0f;
+        public int Reputation; // The dealer's reputation
+        public int ShippingTier; // The dealer's shipping tier
+        public int DealsCompleted; // Number of deals completed with this dealer
+        public List<Drug>? UnlockedDrugs; // List of unlocked drug types with unlocked qualities and effects only
+        public float DebtRemaining; // Remaining debt to the dealer
+        public float DebtPaidThisWeek; // Amount paid to the dealer this week
+
+        public DealerSaveData()
+        {
+            Reputation = 0;
+            ShippingTier = 0;
+            DealsCompleted = 0;
+            UnlockedDrugs = new List<Drug>();
+            DebtRemaining = 0f;
+            DebtPaidThisWeek = 0f;
+        }
     }
+
 }
